58233ead8e3f37d07d0a8e22c602664189e5455c
archie-willett <archie.willett17@imperial.ac.uk>
2023-11-08 19:03:29 +0000

    added comments to explain MayaVi code

--- mayavi_test.py
+++ mayavi_test.py
@@ -17,11 +17,12 @@ Galileo_CA = closest_approach_data(Galileo)
 common_lim = 10
 
 mlab.figure(bgcolor=(1, 1, 1))  # Make background white.
-axis_x = [-common_lim, -common_lim, -common_lim, -common_lim,  common_lim,  common_lim,  common_lim, common_lim]
-axis_y = [-common_lim,  common_lim, -common_lim,  common_lim, -common_lim,  common_lim, -common_lim, common_lim]
-axis_z = [-common_lim, -common_lim,  common_lim,  common_lim, -common_lim, -common_lim,  common_lim, common_lim]
 
-axis = mlab.plot3d(axis_x, axis_y, axis_z, opacity=0, line_width=0.01, tube_radius=0.1, color=(1,1,1))
+# Draws transparent pipe spanning the desired size for the axes because otherwise axes only stretch to span the next plotted thing
+axis = [-common_lim, common_lim]
+axis = mlab.plot3d(axis, axis, axis, opacity=0, line_width=0.01, tube_radius=0.1, color=(1,1,1))
+
+# Draws the axes
 axes = mlab.axes(color=(0, 0, 0), ranges=(-common_lim, common_lim, -common_lim, common_lim, -common_lim, common_lim), nb_labels=5)
 axes.title_text_property.color = (0.0, 0.0, 0.0)
 axes.title_text_property.font_family = 'times'
@@ -29,22 +30,27 @@ axes.label_text_property.color = (0.0, 0.0, 0.0)
 axes.label_text_property.font_family = 'times'
 mlab.outline(color=(0, 0, 0))
 
-callisto = mlab.points3d(0,0,0, color=(0,0,0), resolution=256, scale_factor=2)
+# plots sphere of specified radius
+radius = 1
+sphere = mlab.points3d(0,0,0, color=(0,0,0), resolution=256, scale_factor=2*radius)
 
+# defines 21 equally spaced colours around edge of colour wheel
 colors_x = [1, 1, 1, 1, 6/7, 4/7, 2/7, 0, 0, 0, 0, 0, 0, 0, 0, 2/7, 4/7, 6/7, 1, 1, 1]
 colors_y = [0, 2/7, 4/7, 6/7, 1, 1, 1, 1, 1, 1, 1, 6/7, 4/7, 2/7, 0, 0, 0, 0, 0, 0, 0]
 colors_z = [0, 0, 0, 0, 0, 0, 0, 0, 2/7, 4/7, 6/7, 1, 1, 1, 1, 1, 1, 1, 6/7, 4/7, 2/7]
 colors = np.array([colors_x, colors_y, colors_z])
 colors = np.transpose(colors)
-print(colors[1][0])
 
+# plots all 21 orbits
 i = 0
 for orbit, vector in Juice.items():
     x = vector[1] / R_C ; y = vector[2] / R_C ; z = vector[3] / R_C 
     trajectory = mlab.plot3d(x, y, z,line_width=0.01, tube_radius=0.1, color=(colors[i][0], colors[i][1], colors[i][2]))
     i += 1
 
-# mlab.savefig("vector_plot_in_3d.pdf")
+# makes size of objects independent from distance from the camera position
 mlab.gcf().scene.parallel_projection = True  # Source: <<https://stackoverflow.com/a/32531283/2729627>>.
+# switches on axes orientation indicator
 mlab.orientation_axes()  # Source: <<https://stackoverflow.com/a/26036154/2729627>>.
+# shows plot
 mlab.show()
