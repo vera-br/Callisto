0bf14cf2579d2e6ac78b3a4abf81a323d894367a
miko-o <79320365+vera-br@users.noreply.github.com>
2023-12-12 17:27:20 +0000

    added total field

--- test.py
+++ test.py
@@ -13,11 +13,13 @@ from current_sheet import *
 juice_wrt_callisto_cphio = get_spice_data("juice", "callisto", "cphio", "J")
 juice_wrt_callisto_cphio_CA = closest_approach_data_J("juice", "callisto", "cphio", "J")
 juice_wrt_jupiter_SIII = get_spice_data("juice", "jupiter", "SIII", "J")
+juice_wrt_jupiter_SIII_mag = get_spice_data("juice", "jupiter", "SIII_mag", "J")
 #callisto_wrt_jupiter_SIII = get_spice_data("callisto", "jupiter", "SIII", "J")
 
 # specify orbit
 orbit_cphio = juice_wrt_callisto_cphio["orbit5"]
 orbit_SIII = juice_wrt_jupiter_SIII["orbit5"]
+orbit_SIII_mag = juice_wrt_jupiter_SIII_mag["orbit5"]
 orbit_CA = juice_wrt_callisto_cphio_CA["CA_orbit5"]
 
 # convert time into Timestamp format
@@ -45,7 +47,7 @@ B_ext_mag = pd.Series(dict(zip(time, B_ext_mag)))
 
 
 # current sheet
-rho, z, B_sheet = B_disk(orbit_SIII, R0=R0, R1=R1, D=D, I_constant=Icon)
+rho, z, B_sheet = B_disk(orbit_SIII_mag, R0=R0, R1=R1, D=D, I_constant=Icon)
 
 B_sheet_x = pd.Series(dict(zip(time, B_sheet[:,0])))
 B_sheet_y = pd.Series(dict(zip(time, B_sheet[:,1])))
@@ -56,7 +58,7 @@ B_sheet_mag = pd.Series(dict(zip(time, B_sheet_mag)))
 
 
 # induced field
-B_induced = B_induced_infinite(orbit_cphio, B_external, R_C, R_C - 100)
+B_induced = B_induced_infinite(orbit_cphio, B_external + B_sheet, R_C, R_C - 100)
 #B_induced = B_induced_finite_conductivity(orbit4_cphio, B_external, 0.03, 2*np.pi /(10.1*3600), R_C, R_C - 100, R_C - 200)
 
 B_ind_x = pd.Series(dict(zip(time, B_induced[:,0])))
@@ -72,22 +74,27 @@ B_total_x = pd.Series(dict(zip(time, B_external[:,0] + B_sheet[:,0] + B_induced[
 B_total_y = pd.Series(dict(zip(time, B_external[:,1] + B_sheet[:,1] + B_induced[:,1])))
 B_total_z = pd.Series(dict(zip(time, B_external[:,2] + B_sheet[:,2] + B_induced[:,2])))
 
+
 #---------plotting-----------
 
 fig = plt.figure()
 ax = fig.gca()
 plt.style.use('seaborn-v0_8-whitegrid')
+
+B_total_x.plot(ax=ax, label="Bx", color="royalblue")
+B_total_y.plot(ax=ax, label="By", color="orange")
+B_total_z.plot(ax=ax, label="Bz", color="deeppink")
 '''
 B_ind_mag.plot(ax=ax, label="|B|", color="midnightblue")
 B_ind_x.plot(ax=ax, label="Bx", color="royalblue")
 B_ind_y.plot(ax=ax, label="By", color="orange")
 B_ind_z.plot(ax=ax, label="Bz", color="deeppink")
-'''
+
 B_sheet_mag.plot(ax=ax, label="|B|", color="midnightblue")
 B_sheet_x.plot(ax=ax, label="Bx", color="royalblue")
 B_sheet_y.plot(ax=ax, label="By", color="orange")
 B_sheet_z.plot(ax=ax, label="Bz", color="deeppink")
-'''
+
 B_ext_mag.plot(ax=ax, label="|B|", color="midnightblue")
 B_ext_x.plot(ax=ax, label="Bx", color="royalblue")
 B_ext_y.plot(ax=ax, label="By", color="orange")
@@ -107,5 +114,5 @@ plt.legend()
 ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m-%d %H:%M'))
 
 ax.set_ylabel("B-field [nT]")
-ax.set_title("Plasma Sheet field during Flyby 5")
+ax.set_title("Total field during Flyby 5")
 plt.show()
