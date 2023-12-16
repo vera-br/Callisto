30833a04547dc68eda35dfa9d6c93576f0fea9ff
miko-o <79320365+vera-br@users.noreply.github.com>
2023-12-16 14:15:05 +0100

    added galileo plotting function

--- plot_scripts/plot_field_components.py
+++ plot_scripts/plot_field_components.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 from pandas import Timestamp
-from datetime import datetime
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
 # define constants
@@ -51,7 +51,51 @@ def plot_time_evolution(B_field, orbit_cphio, orbit_CA, flyby_n, field_type):
     ax.axvline(x=time_CA, color='dimgrey', linestyle=":", zorder=0)
 
     # Format the time on the x-axis to include minutes
-    #ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m-%d %H:%M'))
+    ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m-%d %H:%M'))
+
+    ax.set_ylabel("B-field [nT]")
+    ax.set_title(field_type + " field during Flyby %s" % (flyby_n))
+    plt.show()
+
+
+
+def plot_time_evolution_Gal(B_field, orbit_cphio, orbit_CA, flyby_n, field_type):
+
+    # convert time into Timestamp format
+    J2000 = datetime(2000,1,1,12) # difference between J2000 and UTC
+    time = orbit_cphio[0]
+
+    time = [Timestamp((J2000 + timedelta(seconds=timestamp)).strftime('%Y-%m-%d %H:%M:%S')) for timestamp in time]
+    time_CA = Timestamp((J2000 + timedelta(seconds=orbit_CA[0])).strftime('%Y-%m-%d %H:%M:%S'))
+
+    # radial distance of juice wrt callisto
+    radial = pd.Series(dict(zip(time, orbit_cphio[4] / R_C)))
+
+    B_field_x = pd.Series(dict(zip(time, B_field[:,0])))
+    B_field_y = pd.Series(dict(zip(time, B_field[:,1])))
+    B_field_z = pd.Series(dict(zip(time, B_field[:,2])))
+
+    B_field_mag = (B_field[:,0]**2 + B_field[:,1]**2 + B_field[:,2]**2)**0.5
+    B_field_mag = pd.Series(dict(zip(time, B_field_mag)))
+
+    fig = plt.figure()
+    ax = fig.gca()
+    plt.style.use('seaborn-v0_8-whitegrid')
+
+    B_field_mag.plot(ax=ax, label="|B|", color="midnightblue")
+    B_field_x.plot(ax=ax, label="Bx", color="royalblue")
+    B_field_y.plot(ax=ax, label="By", color="orange")
+    B_field_z.plot(ax=ax, label="Bz", color="deeppink")
+
+    plt.legend()
+
+    # plot radial distance of juice wrt callisto
+    radial.plot(ax=ax, label="distance", color="k", secondary_y=True)
+    ax.right_ax.set_ylabel("Radial distance [R_C]")
+    plt.legend(loc="lower right")
+
+    # add a vertical line at CA
+    ax.axvline(x=time_CA, color='dimgrey', linestyle=":", zorder=0)
 
     ax.set_ylabel("B-field [nT]")
     ax.set_title(field_type + " field during Flyby %s" % (flyby_n))
