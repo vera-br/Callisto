fc0bcb44df628a43d4f16b8ed6576c79d157f909
archie-willett <archie.willett17@imperial.ac.uk>
2024-01-22 10:30:15 +0000

    added khurana rotation test file and included function to convert SIII to SIII_mag

--- khurana1997.py
+++ khurana1997.py
@@ -1,6 +1,23 @@
 import numpy as np
+from trajectories.trajectory_analysis import cartesian_to_spherical
 
 R_J = 71492 * 1e3
+
+# rotation angles for the magnetic dipole from the VIP4 model
+theta_VIP4 = np.pi * 9.2 / 180
+phi_VIP4 = np.pi * 158 / 180
+
+def convert_SIII_to_SIII_mag(orbit_SIII):
+    rot_matrix_theta = [[ np.cos(theta_VIP4),   0,   -np.sin(theta_VIP4)], 
+                        [                  0,   1,                    0], 
+                        [ np.sin(theta_VIP4),   0,   np.cos(theta_VIP4)]]
+    rot_matrix_phi = [[ np.cos(phi_VIP4),   np.sin(phi_VIP4),   0], 
+                      [-np.sin(phi_VIP4),   np.cos(phi_VIP4),   0], 
+                      [                0,                  0,   1]]
+    orbit_SIII_mag = orbit_SIII
+    orbit_SIII_mag[1:4] = np.dot(rot_matrix_theta, np.dot(rot_matrix_phi, orbit_SIII[1:4]))
+    orbit_SIII_mag[4:] = cartesian_to_spherical(orbit_SIII_mag[1:4].transpose()).transpose()
+    return orbit_SIII_mag
 
 def B_sheet_khurana(orbit_JSO, orbit_SIII_mag, orbit_SIII):
     x_JSO = orbit_JSO[1] / R_J
@@ -72,16 +89,14 @@ def B_sheet_khurana(orbit_JSO, orbit_SIII_mag, orbit_SIII):
     B_z = (df_drho * dg_dpsi - df_dpsi * dg_drho) / rho
     
     B_cyl = [B_rho, B_psi, B_z]
-
-    # rotation angles for the magnetic dipole from the VIP4 model
-    theta_VIP4 = np.pi * 9.2 / 180
-    phi_VIP4 = np.pi * 158 / 180
-
-    # lines commented out checking if the matrix is the right orientation using the transpose
-    #rot_matrix_theta = np.transpose([[np.cos(theta_VIP4), 0, -np.sin(theta_VIP4)], [0, 1, 0], [np.sin(theta_VIP4), 0, np.cos(theta_VIP4)]])
-    #rot_matrix_phi = np.transpose([[np.cos(phi_VIP4), -np.sin(phi_VIP4), 0], [np.sin(phi_VIP4), np.cos(phi_VIP4), 0], [0, 0, 1]])
-    rot_matrix_theta = [[np.cos(theta_VIP4), 0, -np.sin(theta_VIP4)], [0, 1, 0], [np.sin(theta_VIP4), 0, np.cos(theta_VIP4)]]
-    rot_matrix_phi = [[np.cos(phi_VIP4), -np.sin(phi_VIP4), 0], [np.sin(phi_VIP4), np.cos(phi_VIP4), 0], [0, 0, 1]]
+    
+    rot_matrix_theta = [[ np.cos(theta_VIP4),   0,   np.sin(theta_VIP4)], 
+                        [                  0,   1,                    0], 
+                        [-np.sin(theta_VIP4),   0,   np.cos(theta_VIP4)]]
+    
+    rot_matrix_phi = [[ np.cos(phi_VIP4),  -np.sin(phi_VIP4),   0], 
+                      [ np.sin(phi_VIP4),   np.cos(phi_VIP4),   0], 
+                      [                0,                  0,   1]]
     
     # angles from SIII frame for cartesian to spherical transformation
     theta_SIII = orbit_SIII[5]
@@ -89,8 +104,9 @@ def B_sheet_khurana(orbit_JSO, orbit_SIII_mag, orbit_SIII):
 
     B_spher_SIII = []
     for psi_i, B_cyl_i, theta_i, phi_i in zip(psi, np.transpose(B_cyl), theta_SIII, phi_SIII):
-        #rot_matrix_cyl_cart = np.transpose([[np.cos(psi_i), np.sin(psi_i), 0], [-np.sin(psi_i), np.cos(psi_i), 0], [0, 0, 1]])
-        rot_matrix_cyl_cart = [[np.cos(psi_i), np.sin(psi_i), 0], [-np.sin(psi_i), np.cos(psi_i), 0], [0, 0, 1]]
+        rot_matrix_cyl_cart = [[ np.cos(psi_i),  -np.sin(psi_i),   0], 
+                               [ np.sin(psi_i),   np.cos(psi_i),   0], 
+                               [             0,               0,   1]]
 
         # cylindrical to cartesian transformation in magnetodisc coords.
         B_cart_mag_i = np.dot(rot_matrix_cyl_cart, B_cyl_i)
@@ -98,8 +114,9 @@ def B_sheet_khurana(orbit_JSO, orbit_SIII_mag, orbit_SIII):
         # rotation of dipole relating to untilting coord. axis by theta_VIP4 then unrotating by phi_VIP4
         B_cart_SIII_i = np.dot(rot_matrix_phi, np.dot(rot_matrix_theta, B_cart_mag_i))
         
-        #rot_matrix_cart_spher = np.transpose([[np.cos(phi_i) * np.sin(theta_i), np.cos(phi_i) * np.cos(theta_i), -np.sin(phi_i)], [np.sin(phi_i) * np.sin(theta_i), np.sin(theta_i) * np.cos(theta_i), np.cos(phi_i)], [np.cos(theta_i), np.sin(theta_i), 0]])
-        rot_matrix_cart_spher = [[np.cos(phi_i) * np.sin(theta_i), np.cos(phi_i) * np.cos(theta_i), -np.sin(phi_i)], [np.sin(phi_i) * np.sin(theta_i), np.sin(theta_i) * np.cos(theta_i), np.cos(phi_i)], [np.cos(theta_i), np.sin(theta_i), 0]]
+        rot_matrix_cart_spher = [[ np.cos(phi_i) * np.sin(theta_i),     np.cos(phi_i) * np.cos(theta_i),  -np.sin(phi_i)], 
+                                 [ np.sin(phi_i) * np.sin(theta_i),   np.sin(theta_i) * np.cos(theta_i),   np.cos(phi_i)], 
+                                 [                 np.cos(theta_i),                     np.sin(theta_i),               0]]
         
         # cartesian to spherical transformation in SIII coords.
         B_spher_SIII_i = np.dot(rot_matrix_cart_spher, B_cart_SIII_i)
---
+++ khurana_rot_test.py
@@ -0,0 +1,26 @@
+from trajectories.trajectory_analysis import *
+from field_functions import *
+from khurana1997 import *
+
+callisto_jupiter_SIII = find_nearest_trajectories_G('callisto', 'jupiter', 'SIII1965')
+callisto_jupiter_SIII_mag = find_nearest_trajectories_G('callisto', 'jupiter', 'SIII_mag')
+
+flyby_n = 2
+orbit_cal_SIII = callisto_jupiter_SIII["orbit%s" % (flyby_n)]
+orbit_cal_SIII_mag = callisto_jupiter_SIII_mag["orbit%s" % (flyby_n)]
+orbit_cal_SIII_mag_calc = convert_SIII_to_SIII_mag(orbit_cal_SIII)
+
+fig, ax = plt.subplots(2,3)
+ax[0,0].plot(orbit_cal_SIII_mag[0], orbit_cal_SIII_mag[1])
+ax[0,0].plot(orbit_cal_SIII_mag_calc[0], orbit_cal_SIII_mag_calc[1])
+ax[0,1].plot(orbit_cal_SIII_mag[0], orbit_cal_SIII_mag[2])
+ax[0,1].plot(orbit_cal_SIII_mag_calc[0], orbit_cal_SIII_mag_calc[2])
+ax[0,2].plot(orbit_cal_SIII_mag[0], orbit_cal_SIII_mag[3])
+ax[0,2].plot(orbit_cal_SIII_mag_calc[0], orbit_cal_SIII_mag_calc[3])
+ax[1,0].plot(orbit_cal_SIII_mag[0], orbit_cal_SIII_mag[4])
+ax[1,0].plot(orbit_cal_SIII_mag_calc[0], orbit_cal_SIII_mag_calc[4])
+ax[1,1].plot(orbit_cal_SIII_mag[0], orbit_cal_SIII_mag[5])
+ax[1,1].plot(orbit_cal_SIII_mag_calc[0], orbit_cal_SIII_mag_calc[5])
+ax[1,2].plot(orbit_cal_SIII_mag[0], orbit_cal_SIII_mag[6])
+ax[1,2].plot(orbit_cal_SIII_mag_calc[0], orbit_cal_SIII_mag_calc[6])
+plt.show()
